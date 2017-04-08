c7c0301c85462821f56759417f0bcfedf84e6f90
Anthony Bär <anthonyb.baer@gmail.com>
2017-04-08 21:19:43 +0200

    Update redditPredict.py
    Now writes usernames to a csv file and checks if a user is already on the list (as in has already been "scanned") before doing anything. Currently has a small bug that I'll fix tomorrow.

--- redditPredict.py
+++ redditPredict.py
@@ -7,6 +7,21 @@ id = ""
 secret = ""
 agent = ""
 
+def checkRedditor(username):
+	userlist = []
+	with open("users.txt", "r") as stream:
+		reader = csv.reader(stream)
+		for entry in reader:
+			userlist.append(entry)
+	if (username in userlist):
+		print("Redditor already added to database.")
+	else:
+		with open("users.txt", "w") as saver:
+			writer = csv.writer(saver)
+			writer.writerow(userlist)
+		saveReddits(username)	
+
+
 def saveReddits(username):
 	subreddits = []
 	user = reddit.redditor(username)
@@ -16,8 +31,8 @@ def saveReddits(username):
 	subreddits = list(set(subreddits))
 	subreddits.insert(0, username)
 
-	with open("data.csv", "w") as file:
-		writer = csv.writer(file)
+	with open("data.csv", "w") as stream:
+		writer = csv.writer(stream)
 		writer.writerow(subreddits)
 
 	print("Done")	
@@ -29,4 +44,4 @@ print("Logging in...")
 reddit = praw.Reddit(client_id=id, client_secret=secret, password=pwd, user_agent=agent, username=usr)
 print("Logged in as " + str(reddit.user.me()))
 
-saveReddits(username)
+checkRedditor(username)
