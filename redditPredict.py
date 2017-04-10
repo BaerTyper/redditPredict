d22e9d83d047a7c80f121720f7ec688f7b6448b9
Anthony Bär <anthonyb.baer@gmail.com>
2017-04-10 20:19:18 +0200

    Update redditPredict.py
    More pickling and slightly better code structure.

--- redditPredict.py
+++ redditPredict.py
@@ -1,5 +1,6 @@
 import praw
 import csv
+import pickle
 
 usr = ""
 pwd = ""
@@ -7,7 +8,7 @@ id = ""
 secret = ""
 agent = ""
 
-def checkRedditor(username):
+def checkRedditor(username): # Saves username in "users.p" file, then calls saveReddits
 	try:
 		userlist = pickle.load(open("users.p", "rb"))
 		print(userlist)
@@ -17,27 +18,39 @@ def checkRedditor(username):
 			print("Adding...")
 			userlist.append(username)
 			pickle.dump(userlist, open("users.p", "wb"))
-			#saveReddits(username)
+			saveReddits(username)
 	except:
 		userlist = []
 		userlist.append(username)
 		pickle.dump(userlist, open("users.p", "wb"))
+		saveReddits(username)
 
-def saveReddits(username):
-	subreddits = []
-	user = reddit.redditor(username)
-	for comment in user.comments.new(limit=None):
-		subreddits.append(str(comment.subreddit))
+def saveReddits(username): # Checks usernames subreddits and saves them to "data.p"
+	try:
+		mainList = pickle.load(open("data.p", "rb"))
+		subreddits = []
+		user = reddit.redditor(username)
+		for comment in user.comments.new(limit=None):
+			subreddits.append(str(comment.subreddit))
 
-	subreddits = list(set(subreddits))
-	subreddits.insert(0, username)
+		subreddits = list(set(subreddits))
+		subreddits.insert(0, username)
 
-	with open("data.csv", "w") as stream:
-		writer = csv.writer(stream)
-		writer.writerow(subreddits)
+		mainList.append(subreddits)
+		pickle.dump(mainList, open("data.p", "wb"))
+	except:
+		mainList = []
+		subreddits = []
+		user = reddit.redditor(username)
+		for comment in user.comments.new(limit=None):
+			subreddits.append(str(comment.subreddit))
 
+		subreddits = list(set(subreddits))
+		subreddits.insert(0, username)
+
+		mainList.append(subreddits)
+		pickle.dump(mainList, open("data.p", "wb"))	
 	print("Done.")	
-
 
 username = usr
 
